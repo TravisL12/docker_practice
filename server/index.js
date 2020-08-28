e0088d3619c58c4b794343cea849aa9fa0a96388
Travis Lawrence <travis@instabase.com>
2020-08-28 15:27:00 -0700

    mysql connecting

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+// const migration = require('mysql-migrations');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.APP_PORT;
+const HOST = 'mysql-db';
+const connection = mysql.createConnection({
+  host: `${HOST}`,
+  port: '3306',
+  user: 'root',
+  password: 'password',
+  database: 'db',
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+app.get('/', (req, res) => {
+  res.send({ data: 'Hi Trav' });
+});
+
+app.listen(PORT);
+console.log(`Running on http://${HOST}:${PORT}`);
