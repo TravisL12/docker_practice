85cdb4e596e5cc64a0ee30187e23c3d1065124e9
Travis Lawrence <travis@instabase.com>
2020-08-28 17:28:26 -0700

    Finally got a db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-// const migration = require('mysql-migrations');
 
 const app = express();
 app.use(cors());
---
+++ server/migrations/1598660702373_create_table_users.js
@@ -0,0 +1,5 @@
+module.exports = {
+  up:
+    'CREATE TABLE users (user_id INT NOT NULL, UNIQUE KEY user_id (user_id), name TEXT )',
+  down: 'DROP TABLE users',
+};
