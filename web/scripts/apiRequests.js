8729e45972905c4523aa1d2e62862380a6d12a72
Travis Lawrence <travis.lawrence12@gmail.com>
2020-09-03 23:24:06 -0700

    Breakout search results, ugh

--- web/scripts/apiRequests.js
+++ web/scripts/apiRequests.js
@@ -1,6 +1,14 @@
 import { buildTable } from "./table.js";
 import { buildCalendarData } from "./calendars.js";
 
+const colors = [
+  "hsl(120, 50%, 80%)",
+  "hsl(10, 38%, 80%)",
+  "hsl(175, 75%, 80%)",
+  "hsl(61, 60%, 80%)",
+  "hsl(278, 32%, 80%)",
+  "hsl(51, 34%, 80%)",
+];
 const searches = {};
 
 export async function getData(query = "") {
@@ -9,7 +17,11 @@ export async function getData(query = "") {
     .then(({ data }) => {
       // buildTable([...data]);
       if (query) {
-        searches[query] = data;
+        searches[query] = data.map((d) => ({
+          ...d,
+          query,
+          color: colors[Object.keys(searches).length],
+        }));
         buildCalendarData(Object.values(searches).flat());
       } else {
         buildCalendarData(data);
--- web/scripts/calendars.js
+++ web/scripts/calendars.js
@@ -7,39 +7,47 @@ import {
 } from "./utilities.js";
 const calendars = document.querySelector(".calendars");
 
-export function buildCalendarData(data) {
-  calendars.innerHTML = "";
-  const dates = {};
-  data.forEach((trans) => {
-    const { day, month, year } = splitDate(trans.date);
-    dates[year] = dates[year] || {};
-    dates[year][month] = dates[year][month] || {};
-    dates[year][month][day] = dates[year][month][day] || [];
-    dates[year][month][day].push(trans);
-  });
-  console.log(dates);
-
-  const calendarMap = Object.keys(dates).reduce((acc, year) => {
-    const yearData = dates[year];
-    const monthsData = Object.keys(yearData).map((month) => {
-      return { year: +year, month: +month, days: yearData[month] };
-    });
-    return acc.concat(monthsData);
-  }, []);
-
-  for (let i = 0; i < calendarMap.length; i++) {
-    buildMonth(calendarMap[i]);
-  }
-}
-
-function buildDay(dayData, dayValue, startDow) {
+function buildDay(dayData = [], dayValue, startDow) {
   const dayEl = createElement("div", { classList: "day" });
-  const amount = dayData ? sum(dayData) : 0;
-  if (dayValue - 1 === 0) dayEl.style["gridColumnStart"] = startDow + 1;
+  if (dayValue - 1 === 0) {
+    dayEl.style["gridColumnStart"] = startDow + 1;
+  }
 
-  const totalEl = amount
+  const amount = dayData ? sum(dayData) : 0;
+
+  let totalEl = amount
     ? `<div class="total">${currencyRounded(amount)}</div>`
     : "";
+  if (dayData) {
+    const queries = dayData.reduce((acc, transaction) => {
+      if (!transaction.query) return acc;
+
+      if (!acc[transaction.query]) {
+        acc[transaction.query] = {
+          transactions: [],
+          total: 0,
+          color: transaction.color,
+        };
+      }
+
+      acc[transaction.query].transactions.push(transaction);
+      acc[transaction.query].total += +transaction.amount;
+
+      return acc;
+    }, {});
+
+    if (Object.keys(queries).length > 0) {
+      totalEl = "";
+      Object.keys(queries).forEach((query) => {
+        const { color, total, transactions } = queries[query];
+        totalEl += `
+          <li><span style="background: ${color};" class="query-accent"></span>${query} - ${currencyRounded(
+          total
+        )} (${transactions.length})</li>`;
+      });
+      totalEl = `<ul class="query-list">${totalEl}</ul>`;
+    }
+  }
 
   dayEl.innerHTML = `
     <div class="date">${dayValue}</div>
@@ -80,4 +88,29 @@ function buildMonth(data) {
   monthWrapperEl.appendChild(titleEl);
   monthWrapperEl.appendChild(monthEl);
   calendars.appendChild(monthWrapperEl);
+}
+
+export function buildCalendarData(data) {
+  calendars.innerHTML = "";
+  const dates = {};
+  data.forEach((trans) => {
+    const { day, month, year } = splitDate(trans.date);
+    dates[year] = dates[year] || {};
+    dates[year][month] = dates[year][month] || {};
+    dates[year][month][day] = dates[year][month][day] || [];
+    dates[year][month][day].push(trans);
+  });
+  console.log(dates);
+
+  const calendarMap = Object.keys(dates).reduce((acc, year) => {
+    const yearData = dates[year];
+    const monthsData = Object.keys(yearData).map((month) => {
+      return { year: +year, month: +month, days: yearData[month] };
+    });
+    return acc.concat(monthsData);
+  }, []);
+
+  for (let i = 0; i < calendarMap.length; i++) {
+    buildMonth(calendarMap[i]);
+  }
 }
