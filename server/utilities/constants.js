78b410a346210001f6f061c68c3e6241ff8a61bc
Travis Lawrence <travis.lawrence12@gmail.com>
2021-11-26 15:37:40 -0800

    stuff

--- server/utilities/constants.js
+++ server/utilities/constants.js
@@ -1,5 +1,5 @@
 const TRANSACTION_QUERY = `SELECT 
-        t.description, t.payee, t.date, t.amount, cat.name category, subcat.name subcategory from transactions t
+        t.id, t.description, t.payee, t.date, t.amount, cat.name category, subcat.name subcategory from transactions t
         left join categories cat on t.category_id = cat.id
         left join categories subcat on t.sub_category_id = subcat.id
       where description not like 'ONLINE TRANSFER TO%'`;
